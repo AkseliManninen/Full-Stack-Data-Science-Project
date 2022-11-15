6a9ca8ba3c53bbb068e075cdf3d6b0ea898d0122
Akseli Manninen <akseli.manninen@aalto.fi>
2022-11-15 22:13:57 +0200

    Lambda writes to S3

--- FetchData.py
+++ FetchData.py
@@ -1,7 +1,51 @@
 import boto3
+import urllib3
+import requests
+import json
+
+# Get the token for the API
+token = "djnK2W5vhk3W4fDZIZA9ka3Blr4ncJsv4R3mgFYK"
+
+# Header for the api
+headers = {'x-api-key': token}
+
+# Randomly selected start and end times
+start_time = "2022-10-25T00:00:00+03:00"
+end_time = "2022-10-26T00:00:00+03:00"
+
+query = {
+        "start_time": start_time,
+        "end_time": end_time,
+        }
+#encoded = urllib.parse.urlencode(query)
+
+# URL for the API, where 124 is the variableId
+url = "https://api.fingrid.fi/v1/variable/124/events/csv?" 
+
+response = requests.get(url, headers=headers, params = query)
+
+print(response.status_code)
+
+
 def lambda_handler(event, context):
-    result = "Hello World"
+
+    string = response.text
+    bucket_name = "electricity-data-bucket"
+    file_name = "electricity_data.csv"
+    s3_path = file_name
+
+    s3 = boto3.resource("s3")
+    s3.Bucket(bucket_name).put_object(Key=s3_path, Body=string)
     return {
-        'statusCode' : 200,
-        'body': result
+        'statusCode': 200,
     }
+
+
+
+    
+
+
+
+
+
+    
--- Lambda.tf
+++ Lambda.tf
@@ -5,33 +5,18 @@ data "archive_file" "python_lambda_package" {
   output_path = "lambda_function.zip"
 }
 
-resource "aws_iam_role" "iam_role_for_lambda" {
-  name = "iam_role_for_lambda"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+data "aws_iam_role" "iam_role_for_lambda2" {
+  name = "lambda-upload-s3-akseli"
 }
 
 resource "aws_lambda_function" "FetchData" {
   filename      = "lambda_function.zip"
   function_name = "fetch_data_lambda"
-  role          = aws_iam_role.iam_role_for_lambda.arn
+  role          = data.aws_iam_role.iam_role_for_lambda2.arn
+  #role          = aws_iam_role.iam_role_for_lambda.arn # This is how the role would have been assignned if it was created in the same file.
   handler       = "FetchData.lambda_handler" 
   layers        = ["arn:aws:lambda:eu-west-1:065739622999:layer:python_layer:1"]
-  source_code_hash = filebase64sha256("lambda_function.zip")
+  source_code_hash = filebase64sha256("lambda_function.zip") # Needed to have to include the changes in the python file to the lambda function.
 
   runtime = "python3.8"
 
@@ -42,5 +27,31 @@ resource "aws_lambda_function" "FetchData" {
   }
 }
 
+# Implementation ends here
+
+# ---------------------------------------------------------------------------------------------------------------------------
+
 
 # terraform init > terraform plan > terraform apply
+
+# Example role for lambda function
+
+#resource "aws_iam_role" "iam_role_for_lambda" {
+#  name = "iam_role_for_lambda"
+
+#  assume_role_policy = <<EOF
+#{
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Action": "sts:AssumeRole",
+#      "Principal": {
+#        "Service": "lambda.amazonaws.com"
+#      },
+#      "Effect": "Allow",
+#      "Sid": ""
+#    }
+#  ]
+#}
+#EOF
+#}
