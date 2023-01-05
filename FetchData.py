088a5ebc621b13151c57e82dbf9cc205f60f75c4
Akseli <akseli.manninen@gmail.com>
2023-01-05 17:34:09 +0200

    Added more variables to be pulled with Lambda functions

--- FetchData.py
+++ FetchData.py
@@ -4,18 +4,23 @@ import boto3
 import requests
 from datetime import date
 from datetime import timedelta
+import pandas as pd
+from pandas.io.common import StringIO
+import numpy as np
 
 yesterday = date.today() - timedelta(days=1)
 yesterday_str = yesterday.strftime("%Y-%m-%d")
 
+today_str = date.today().strftime("%Y-%m-%d")
+
 # Get the token for the API
-token = "djnK2W5vhk3W4fDZIZA9ka3Blr4ncJsv4R3mgFYK"
+token = ## Add token here
 
 # Header for the api
 headers = {'x-api-key': token}
 
 start_time = yesterday_str + "T00:00:00+00:00"
-end_time = yesterday_str + "T23:00:00+00:00"
+end_time = today_str + "T23:00:00+00:00"
 
 
 query = {
@@ -23,28 +28,46 @@ query = {
         "end_time": end_time,
         }
 
-# URL for the API, where 124 is the variableId
-url = "https://api.fingrid.fi/v1/variable/124/events/csv?" 
+variables = ["74", "124", "165", "166", "241", "242"]
+dataframes = []
 
-response = requests.get(url, headers=headers, params = query)
+for variable in variables:
+    url = f"https://api.fingrid.fi/v1/variable/{variable}/events/csv?"
 
-print(response.status_code)
+    response = requests.get(url, headers=headers, params = query)
+
+    # Creates a file-like object from the string
+    file = StringIO(response.text)
+
+    # Read the CVS data from the file-like object int a Pandas dataframe
+    df = pd.read_csv(file)
+
+    df.rename(columns = {'value': variable}, inplace= True)
+
+    dataframes.append(df)
+
+data = dataframes[0]
+dataframes = dataframes[1:]
+
+# Combines the dataframes based on start and end times
+for dataframe in dataframes:
+        data = pd.merge(data, dataframe, on = ["start_time", "end_time"], how = "outer")
+
+# Modifies the Pandas dataframe into a csv file
+
+# header = False pystyy säätäää, ettei tuu otsikot mukaa
+data = data.to_csv(index = False, header = False)
 
 bucket_name = "electricity-data-bucket"
-file_name = "electricity_data.csv"
+file_name = "electricity_data_23.csv"
 s3_path = file_name
-
-string = response.text
 
 s3 = boto3.resource("s3")
 
 # get existing data from s3
 current_data = boto3.client('s3').get_object(Bucket = bucket_name, Key = s3_path)['Body'].read().decode('utf-8')
-    
-string = string.splitlines(True)[1:]
-string = ''.join(string)
 
-append_data = str(current_data) + string
+append_data = str(current_data) + str(data)
     
 s3.Bucket(bucket_name).put_object(Key=s3_path, Body=append_data)
 
@@ -54,6 +77,7 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
     }
+
 
 
 
--- FetchLongData.py
+++ FetchLongData.py
@@ -2,37 +2,63 @@
 
 import boto3
 import requests
+import numpy as np
+import pandas as pd
+from pandas.io.common import StringIO
 
 # Get the token for the API
-token = "djnK2W5vhk3W4fDZIZA9ka3Blr4ncJsv4R3mgFYK"
+token = ## Add token here
 
-# Header for the api
+# Headers for the api
 headers = {'x-api-key': token}
 
-# Randomly selected start and end times
-start_time = "2020-01-01T00:00:00+00:00"
-end_time = "2022-11-16T23:00:00+00:00"
+# Selected start and end times
+#start_time = "2020-01-01T00:00:00+00:00"
+start_time = "2023-01-02T00:00:00+00:00"
+end_time = "2023-01-04T23:00:00+00:00"
 
 query = {
         "start_time": start_time,
         "end_time": end_time,
         }
 
-# URL for the API, where 124 is the variableId
-url = "https://api.fingrid.fi/v1/variable/124/events/csv?" 
+# Variables
+variables = ["74", "124", "165", "166", "241", "242"]
+dataframes = []
 
-response = requests.get(url, headers=headers, params = query)
+for variable in variables:
+    url = f"https://api.fingrid.fi/v1/variable/{variable}/events/csv?"
+
+    response = requests.get(url, headers=headers, params = query)
+
+    # Creates a file-like object from the string
+    file = StringIO(response.text)
+
+    # Read the CVS data from the file-like object int a Pandas dataframe
+    df = pd.read_csv(file)
+    
+    df.rename(columns = {'value': variable}, inplace= True)
+
+    dataframes.append(df)
+
+data = dataframes[0]
+dataframes = dataframes[1:]
+
+# Combines the dataframes based on start and end times
+for dataframe in dataframes:
+        data = pd.merge(data, dataframe, on = ["start_time", "end_time"], how = "outer")
+
+# Modifies the Pandas dataframe into a csv file
+data = data.to_csv(index = False, header = True)
 
 print(response.status_code)
 
 bucket_name = "electricity-data-bucket"
-file_name = "electricity_data.csv"
+file_name = "electricity_data_23.csv"
 s3_path = file_name
 
-string = response.text
-
 s3 = boto3.resource("s3")
-s3.Bucket(bucket_name).put_object(Key=s3_path, Body=string)
+s3.Bucket(bucket_name).put_object(Key=s3_path, Body=data)
 
 def lambda_handler(event, context):
     return {
